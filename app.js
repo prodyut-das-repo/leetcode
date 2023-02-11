87309aa81eae829341e72390b2feaa7c9159fb7d
Prodyut <prodyut8765@gmail.com>
2023-02-12 00:41:58 +0530

    12. Integer to Roman

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 /**
- * @param {string[]} words
+ * @param {number} num
  * @return {string}
  */
-var firstPalindrome = function(words) {
-    for(i of words){
-      if(i === i.split('').reverse().join('')){
-        return i;
+var intToRoman = function (num) {
+  if (num < 5) return;
+  let res = [];
+  let obj = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M",
+  };
+  const keys = Object.keys(obj).reverse();
+  for (let index = 0; index < keys.length; index++) {
+    let floor = Math.floor(num / keys[index]);
+    if (floor > 0) {
+      for (let j = 0; j < floor; j++) {
+        res.push(obj[keys[index]]);
       }
-
     }
-    return '';
+    console.log(num);
+    num -= floor * keys[index];
+  }
+  console.log(res.join(""));
 };
 
-firstPalindrome(["abc","car","ada","racecar","cool"]);
-
+intToRoman(3999);
 
 // Example 1:
-
-// Input: words = ["abc","car","ada","racecar","cool"]
-// Output: "ada"
-// Explanation: The first string that is palindromic is "ada".
-// Note that "racecar" is also palindromic, but it is not the first.
+// Input: num = 3
+// Output: "III"
+// Explanation: 3 is represented as 3 ones.
 // Example 2:
 
-// Input: words = ["notapalindrome","racecar"]
-// Output: "racecar"
-// Explanation: The first and only string that is palindromic is "racecar".
+// Input: num = 58
+// Output: "LVIII"
+// Explanation: L = 50, V = 5, III = 3.
 // Example 3:
 
-// Input: words = ["def","ghi"]
-// Output: ""
-// Explanation: There are no palindromic strings, so the empty string is returned.
+// Input: num = 1994
+// Output: "MCMXCIV"
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
